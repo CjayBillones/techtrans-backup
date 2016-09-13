9e86d637eae2fe8d5fa8dbffd502b4fb62e799a7
Cjay <billonesciprian08@gmail.com>
2016-09-13 16:00:13 +0800

    Setup routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,9 @@ Rails.application.routes.draw do
   resources :users
   get '/register' => 'users#new'
   post '/register' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
 end
