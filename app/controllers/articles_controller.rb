725d9c375da046efd1ea4730e91d1b9e2f06b3c8
Cjay <billonesciprian08@gmail.com>
2016-10-01 17:06:50 +0800

    Setup articles_controller
    Finished article creation

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,50 @@
+class ArticlesController < ApplicationController
+  include ApplicationHelper
+  
+  before_action :logged_in_user, only: [:new, :create, :edit, :update, :destroy]
+  before_action :admin_user, only: [:new, :create, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = current_user.articles.build(article_params)
+
+    if @article.save
+      flash[:success] = "Successfully created article"
+      redirect_to articles_path
+    else
+      render 'new'
+    end    
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id]).destroy
+    flash[:success] = "Successfully deleted article"
+    redirect_to articles_path
+  end
+
+  private
+
+    def article_params
+      params.require(:article).permit(:title, :content)
+    end
+
+end
