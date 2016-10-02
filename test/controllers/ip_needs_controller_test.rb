2812e0047fe04cec0276e79192e63ee2317381cb
Cjay <billonesciprian08@gmail.com>
2016-10-02 16:32:41 +0800

    Add tests for IP Needs controller and model

--- test/controllers/ip_needs_controller_test.rb
+++ test/controllers/ip_needs_controller_test.rb
@@ -2,4 +2,115 @@ require 'test_helper'
 
 class IpNeedsControllerTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user       = users(:cjay)
+    @other_user = users(:dyosa)
+    @ip_need    = ip_needs(:coatin)
+  end
+
+  test "should get index" do
+    get ip_needs_path
+    assert_response :success
+  end
+
+  test "should get show" do
+    get ip_need_path(@ip_need)
+    assert_response :success
+  end
+
+  test "should redirect new when not logged in" do
+    get new_ip_need_path
+    assert_redirected_to login_path
+  end
+
+  test "should get new when user is logged in" do
+    log_in_as(@user)
+    get new_ip_need_path
+    assert_response :success
+  end
+
+  test "should redirect create when not logged in" do
+    post ip_needs_path, params: { ip_need: {
+                                            title:       "Lorem Ipsum",
+                                            description: "Lorem Ipsum"
+                                        }}
+    assert_redirected_to login_path
+  end
+
+  test "should create IP need when user is logged in" do
+    log_in_as(@user)
+    post ip_needs_path, params: { ip_need: {
+                                            title:       "Lorem Ipsume",
+                                            description: "Lorem Ipsum",
+                                        }}
+    assert_redirected_to @user.ip_needs.first    
+  end
+
+  test "should redirect edit when not logged in" do
+    get edit_ip_need_path(@ip_need)
+    assert_redirected_to login_path
+  end
+
+  test "should redirect edit when wrong user is logged in" do
+    log_in_as(@other_user)
+    get edit_ip_need_path(@ip_need)
+    assert_redirected_to root_path
+  end
+
+  test "should get edit when correct user is logged in" do
+    log_in_as(@user)
+    get edit_ip_need_path(@ip_need)
+    assert_response :success
+  end
+
+  test "should redirect update when not logged in" do
+    patch ip_need_path(@ip_need), params: { ip_need: {
+                                                title:       "Lorem Ipsum",
+                                                description: "Lorem Ipsum"
+                                            }}
+    assert_redirected_to login_path
+  end
+
+  test "should redirect update when wrong user is logged in" do
+    log_in_as(@other_user)
+    patch ip_need_path(@ip_need), params: { ip_need: {
+                                            title:       "Lorem Ipsum",
+                                            description: "Lorem Ipsum"
+                                        }}
+    assert_redirected_to root_path
+  end
+
+  test "should update when correct user is logged in" do
+    log_in_as(@user)
+    patch ip_need_path(@ip_need), params: { ip_need: {
+                                                title:       "Lorem Ipsum",
+                                                description: "Lorem Ipsum"
+                                            }}
+    assert_redirected_to @ip_need
+  end
+
+  test "should redirect destroy if not logged in" do
+    assert_no_difference 'IpNeed.count' do
+      delete ip_need_path(@ip_need)
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when wrong user is logged in" do
+    log_in_as(@other_user)
+    assert_no_difference 'IpNeed.count' do
+      delete ip_need_path(@ip_need)
+    end
+    assert_redirected_to root_path
+  end
+
+  test "should destroy when correct user is logged in" do
+    log_in_as(@user)
+    assert_difference 'IpNeed.count', -1 do
+      delete ip_need_path(@ip_need)
+    end
+    assert_redirected_to ip_needs_path
+  end
+
+
 end
--- test/models/ip_need_test.rb
+++ test/models/ip_need_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class IpNeedTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:cjay)
+    @ip_need = @user.ip_needs.build(title:"Lorem Ipsum", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+  end
+
+  test "should be valid" do
+    assert @ip_need.valid?
+  end
+
+  test "user_id should be present" do
+    @ip_need.user_id = nil
+    assert_not @ip_need.valid?
+  end
+
+  test "title should be present" do
+    @ip_need.title = " "
+    assert_not @ip_need.valid?
+  end
+
+  test "title should be at most 255 characters" do
+    @ip_need.title = "a" *256
+    assert_not @ip_need.valid?
+  end
+
+  test "content should be present" do
+    @ip_need.description = " "
+    assert_not @ip_need.valid?
+  end
+
+  test "order should be most recent first" do
+    assert_equal ip_needs(:uprint), IpNeed.first
+  end
+
 end
