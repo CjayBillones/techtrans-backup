92e2e3331d2f39863cabda312c06256429e5da8b
Cjay <billonesciprian08@gmail.com>
2016-10-22 18:06:22 +0800

    Merge masterdyosa to cjay-master

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
-  get '/home' => 'static_pages#home'
+  get '/' => 'static_pages#home'
   get '/about' => 'static_pages#about'
   get '/expertise'=> 'static_pages#expertise'
   get '/itso-ipophl' => 'static_pages#itso_ipophl'
