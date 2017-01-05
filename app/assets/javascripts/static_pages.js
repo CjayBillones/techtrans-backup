6c2c91db85fc36bbc26391e7afd36c0089030ac4
Gerard Montemayor <grbmontemayor@gmail.com>
2017-01-05 23:47:40 +0800

    Add navbar and incomplete splash section

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,26 +1,33 @@
 $(document).ready(function(){
     // Activate Carousel
-    $("#myCarousel").carousel({interval: 500});
+    // $("#myCarousel").carousel({interval: 500});
 
-    // Enable Carousel Indicators
-    $(".item1").click(function(){
-        $("#myCarousel").carousel(0);
-    });
-    $(".item2").click(function(){
-        $("#myCarousel").carousel(1);
-    });
-    $(".item3").click(function(){
-        $("#myCarousel").carousel(2);
-    });
-    $(".item4").click(function(){
-        $("#myCarousel").carousel(3);
-    });
+    // // Enable Carousel Indicators
+    // $(".item1").click(function(){
+    //     $("#myCarousel").carousel(0);
+    // });
+    // $(".item2").click(function(){
+    //     $("#myCarousel").carousel(1);
+    // });
+    // $(".item3").click(function(){
+    //     $("#myCarousel").carousel(2);
+    // });
+    // $(".item4").click(function(){
+    //     $("#myCarousel").carousel(3);
+    // });
 
-    // Enable Carousel Controls
-    $(".left").click(function(){
-        $("#myCarousel").carousel("prev");
-    });
-    $(".right").click(function(){
-        $("#myCarousel").carousel("next");
+    // // Enable Carousel Controls
+    // $(".left").click(function(){
+    //     $("#myCarousel").carousel("prev");
+    // });
+    // $(".right").click(function(){
+    //     $("#myCarousel").carousel("next");
+    // });
+
+    $('.slider').glide({
+        autoplay: false,
+        arrowsWrapperClass: 'slider-arrows',
+        arrowRightText: '',
+        arrowLeftText: ''
     });
 });
