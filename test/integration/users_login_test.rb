c5182bb073a7858ab813056a7239b9269397b95a
Cjay <billonesciprian08@gmail.com>
2017-01-11 09:47:52 +0800

    Add test for login of inactive user

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -18,13 +18,33 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
 
+  test "login inactive user" do
+    get login_path
+    post login_path, params: { session: {email: @inactive_user.email, password: 'password'}}
+    assert_not is_logged_in?
+    assert_not flash.empty?
+    @inactive_user.update!(activated: true, activated_at: Time.zone.now)
+    post login_path, params: { session: {email: @inactive_user.email, password: 'password'}}
+    assert is_logged_in?
+    assert_redirected_to @inactive_user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@inactive_user)
+    delete logout_path
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@inactive_user), count: 0
+  end
+
   test "login unapproved user" do
     get login_path
     post login_path, params: { session: {email: @unapproved_user.email, password: 'password'}}
     assert_not is_logged_in?
     assert_not flash.empty?
     @unapproved_user.update!(approved: true)
-    assert_not flash.empty?
     post login_path, params: { session: {email: @unapproved_user.email, password: 'password'}}
     assert is_logged_in?
     assert_redirected_to @unapproved_user
