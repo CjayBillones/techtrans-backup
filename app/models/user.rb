02ec356a4d23a9c8f1e84acebffbeb13eba8268c
Cjay <billonesciprian08@gmail.com>
2017-01-29 14:31:43 +0800

    Add validation for presence of username upon signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   has_secure_password
   validates :first_name, presence: true
   validates :last_name,  presence: true
+  validates :username, presence: true
   validates :email, presence: true, length: { maximum: 255 }, 
             format: { with: VALID_EMAIL_REGEX }, 
             uniqueness: {case_sensitive: false}
