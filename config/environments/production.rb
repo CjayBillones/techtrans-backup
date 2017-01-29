7def803892eb0a69d7d8cac83a4bc379f55a9db4
Cjay <billonesciprian08@gmail.com>
2017-01-29 15:31:51 +0800

    Change hostname for smtp in production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'ipttu-website.herokuapp.com'
+  host = 'iptechtransfer.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.gmail.com',
