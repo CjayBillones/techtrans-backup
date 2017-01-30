d9bb004ab69ef0c1bec230e51cccb643bcfaa368
Cjay <billonesciprian08@gmail.com>
2017-01-31 02:10:44 +0800

    Add ip rejection functionality for admin

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -10,4 +10,14 @@ class AdminsController < ApplicationController
     redirect_to admin_manage_ips_path()
   end
 
+  def reject_ip
+    ip = (params[:ip_type] == 'IpOffer') ? IpOffer.find(params[:id]) : IpNeed.find(params[:id])
+    
+    if ip
+      ip.update!(approval_status: 'rejected')
+    end
+
+    redirect_to admin_manage_ips_path()
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -67,6 +67,7 @@ Rails.application.routes.draw do
   get '/admin/manage_resources' => 'admin_dashboards#manage_resources'
 
   post '/admin/approve_ip' => 'admins#approve_ip'
+  post '/admin/reject_ip' => 'admins#reject_ip'
 
   get '/dashboard' => 'user_dashboards#index', as: 'user_dashboard'
   get '/dashboard/tags/:tags', to: 'user_dashboards#index', as: :dashboard_tag
