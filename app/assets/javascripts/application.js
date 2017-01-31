7dfe25209d4e824fdc0de6050902461fb43eae7d
Cjay <billonesciprian08@gmail.com>
2017-01-31 21:46:39 +0800

    Fix javascript bug on tabs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,15 +35,4 @@ $(document).on('turbolinks:load', function(){
 
 $(document).on('turbolinks:load', function() {
   $("#industry_classification, #industry_country_code, #academe_user_type, #ip_need_privacy_option, #ip_offer_patent_status").select2();
-  $('#admin-manage-ip .menu .item').tab({
-    context: $('#admin-manage-ip')
-  });
-  $('#admin-manage-users .menu .item').tab({
-    context: $('#admin-manage-users')
-  });
-  $('.ui.checkbox').checkbox();
-  $('.ui.dropdown').dropdown();
-  $('.ui.sticky').sticky({
-    offset: 60
-  });
 });
