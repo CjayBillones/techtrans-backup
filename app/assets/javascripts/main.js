968edaeb430b357487723f79e5a43d319ecb797a
Gerard Montemayor <grbmontemayor@gmail.com>
2017-01-31 23:11:49 +0800

    Add carousel on landing

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
-
+    $('.js-slick').slick({
+    autoplay: true,
+    autoplaySpeed: 5000,
+    dots: true,
+    draggable: false,
+    fade: true,
+    speed: 1000
+  });
     
 });
