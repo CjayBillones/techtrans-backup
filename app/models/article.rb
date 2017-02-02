f7a1a92bc36131bb21b24e19e29c0502abb1b8d5
Cjay <billonesciprian08@gmail.com>
2017-02-02 17:36:59 +0800

    Remove home_display styles for photo attachments from articles, offers, and needs

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,7 +6,7 @@ class Article < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
   validates :content, presence: true
 
-  has_attached_file :banner_photo, :styles => { :home_display => "200x106!"},
+  has_attached_file :banner_photo,
                     :url  => "/assets/articles/:id/images/:style/:basename.:extension",
                     :path => ":rails_root/public/assets/articles/:id/images/:style/:basename.:extension"
   validates_attachment_size :banner_photo, :less_than => 25.megabytes
