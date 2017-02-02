d33ce880a7317fb6787875ae4c90c9d2268e62f8
Cjay <billonesciprian08@gmail.com>
2017-02-02 18:07:08 +0800

    Add customized display style for article, need, and offer image  attachments

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,7 +6,7 @@ class Article < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
   validates :content, presence: true
 
-  has_attached_file :banner_photo,
+  has_attached_file :banner_photo, :styles => { :display => "200x106!"},
                     :url  => "/assets/articles/:id/images/:style/:basename.:extension",
                     :path => ":rails_root/public/assets/articles/:id/images/:style/:basename.:extension"
   validates_attachment_size :banner_photo, :less_than => 25.megabytes
