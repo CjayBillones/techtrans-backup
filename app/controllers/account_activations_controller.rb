1f45ca37df488036b3b9bf72d3140c8c3bb28fe4
Cjay <billonesciprian08@gmail.com>
2017-02-05 20:04:42 +0800

    Redirect to login page after activation

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -4,13 +4,11 @@ class AccountActivationsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
-      login user
       flash[:success] = "Account activated!"
-      redirect_to user
     else
       flash[:danger] = "Invalid activation link"
-      redirect_to login_path
     end
+      redirect_to login_path
   end
 
 end
